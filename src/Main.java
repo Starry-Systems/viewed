7c97f8a7713c3fc6448cbd482d7f3f21204f76ea
meowcat767 <benhouse124@gmail.com>
2025-10-31 16:15:52 +0000

    had to make viewed-src folder

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import javax.swing.*;
+
+public class Main {
+    public static void Main(String[] args) {
+        JFrame window = new JFrame("Viewed Alpha");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // make sure when close button is hit process closes
+        window.setSize(500, 600);
+        window.setVisible(true);
+    }
+}
